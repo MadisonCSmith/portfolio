8d4f73cc068586c847c8df597881224f6bf08cbb
Madison Smith <mady.c.smith@gmail.com>
2019-07-24 07:35:04 -0700

    changed tag line

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -17,7 +17,7 @@ export class HomeView extends Component {
 					<div className="landing">
 						<div id="hi">Hi,</div>
 						<div id="im">I'm Madison Smith,</div>
-						<div id="student">a student at the University of Washington.</div>
+						<div id="student">a recent graduate of the University of Washington.</div>
 						<div id="links">
 							<a href="https://www.linkedin.com/in/madison-c-smith" target="_blank"><img className="links-icon" src={linkedInLogo} alt="linkedin icon"/></a>
 							<a href="https://github.com/MadisonCSmith" target="_blank"><img className="links-icon" src={githubLogo} alt="Github logo"/></a>
